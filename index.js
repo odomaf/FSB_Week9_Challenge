c4dc20a5ddb5047cf4206584b50f0d0a998b9e13
Anne Odom <afodom207@gmail.com>
2026-01-03 16:25:00 -0600

    Update: implement imports, create question array for user input, implement inquirer for presenting user questions and getting answers

--- index.js
+++ index.js
@@ -4,10 +4,13 @@
 // - fs for writing files
 // - inquirer for collecting user input
 // - the generateMarkdown function from ./utils/generateMarkdown
+// const inquirer = require("inquirer");
 
-// const fs = ...
-// const inquirer = ...
-// const generateMarkdown = ...
+import fs from "fs";
+import inquirer from "inquirer";
+import { generateMarkdown } from "./utils/generateMarkdown.js";
+
+console.log("Required and imported all necessary packages");
 
 // TODO: Create an array of questions for user input.
 // Each question should collect part of the README content:
@@ -29,8 +32,60 @@
 // }
 
 const questions = [
-  // TODO: Add your question objects here
+  {
+    type: "input",
+    name: "username",
+    message: "github username: ",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "github email: ",
+  },
+  {
+    type: "input",
+    name: "title",
+    message: "project title: "
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "project description: "
+  },
+  {
+    type: "choice",
+    name: "license",
+    message: "license: "
+  },
+  {
+    title: "input",
+    name: "installCommand",
+    message: "installation command: "
+  },
+  {
+    title: "input",
+    name: "testCommand",
+    message: "test command: "
+  },
+  {
+    title: "input",
+    name: "usageInfo",
+    message: "usage information: "
+  },
+  {
+    title: "input",
+    name: "contributionGuide",
+    message: "contribution guidelines: "
+  }
 ];
+
+inquirer
+  .prompt(questions)
+  .then((data) => {
+    const name = data.name;
+    console.log("Data is: ", data);
+  })
+  .catch((err) => console.error("Something bad happened: ", err));
 
 // TODO: Create a function to write the README file.
 // It should take a file name and the data to write.
